e027eb0715c5ae609efe29a7c3c99142b0128f70
aholster <aholster@student.codam.nl>
2019-04-24 13:23:20 +0200

    cleaned up makefile and file structure
    to increase ease of use all testfunctions will now be assumed to have a
    proprietary header located in a given folder, also containing the test files
    and or library
    
    makefile split into differing fields for ease to editing and removing necesary amount of letters typed per test written

--- ft_strlen.c
+++ ft_strlen.c
@@ -3,14 +3,14 @@
 /*                                                        ::::::::            */
 /*   ft_strlen.c                                        :+:    :+:            */
 /*                                                     +:+                    */
-/*   By: dsaripap <marvin@codam.nl>                   +#+                     */
+/*   By: aholster <aholster@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
-/*   Created: 2019/03/28 17:34:34 by dsaripap      #+#    #+#                 */
-/*   Updated: 2019/04/09 20:28:42 by dsaripap      ########   odam.nl         */
+/*   Created: 2019/03/28 17:34:34 by dsaripap       #+#    #+#                */
+/*   Updated: 2019/04/24 13:11:07 by aholster      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../header.h"
+#include "libft.h"
 
 size_t		ft_strlen(const char *s)
 {
--- tests/header.h
+++ tests/header.h
@@ -3,10 +3,10 @@
 /*                                                        ::::::::            */
 /*   header.h                                           :+:    :+:            */
 /*                                                     +:+                    */
-/*   By: dsaripap <marvin@codam.nl>                   +#+                     */
+/*   By: aholster <aholster@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
-/*   Created: 2019/04/21 16:26:05 by dsaripap      #+#    #+#                 */
-/*   Updated: 2019/04/21 20:26:38 by dsaripap      ########   odam.nl         */
+/*   Created: 2019/04/21 16:26:05 by dsaripap       #+#    #+#                */
+/*   Updated: 2019/04/24 13:23:09 by aholster      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,36 +15,32 @@
 
 # include <stdlib.h>
 # include <string.h>
-# include "../framework/libunit.h"
+# include "libunit.h"
+# include "libft.h"
 
 int		atoi_positive_test();
 int		atoi_doublesign_test();
 int		atoi_minus_test();
-int		ft_atoi(const char *str);
 int		atoi_launcher(void);
 
 int		d_isalpha_test();
 int		one_isalpha_test();
 int		dollar_isalpha_test();
-int		ft_isalpha(int c);
 int		isalpha_launcher(void);
 
 int		six_isdigit_test(void);
 int		w_isdigit_test(void);
 int		four_isdigit_test(void);
-int		ft_isdigit(int c);
 int		isdigit_launcher(void);
 
 int		diff_strcmp_test(void);
 int		same_strcmp_test(void);
 int		empty_strcmp_test(void);
-int		ft_strcmp(const char *s1, const char *s2);
 int		strcmp_launcher(void);
 
 int		barcelona_strlen_test(void);
 int		aristotle_strlen_test(void);
 int		strlen_test03(void);
-size_t	ft_strlen(const char *s);
 int		strlen_launcher(void);
 
 #endif
