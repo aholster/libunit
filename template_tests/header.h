62f8a41ea2d3e492ee29f1346e5a7df1ec0cbd9c
aholster <aholster@student.codam.nl>
2019-04-30 16:21:34 +0200

    integrating templates and laying foundation for usercreated tests

---
+++ template_tests/header.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   header.h                                           :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: aholster <aholster@student.codam.nl>         +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2019/04/21 16:26:05 by dsaripap       #+#    #+#                */
+/*   Updated: 2019/04/29 18:01:24 by aholster      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef HEADER_H
+# define HEADER_H
+
+# include <stdlib.h>
+# include <string.h>
+# include "libunit.h"
+
+
+
+#endif
---
+++ tests/main.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   main.c                                             :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: aholster <aholster@student.codam.nl>         +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2019/04/20 21:49:40 by aholster       #+#    #+#                */
+/*   Updated: 2019/04/29 14:39:23 by aholster      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "header.h"
+
+int	main(int argc, char **argv)
+{
+	t_unit	*lst;
+	
+	loadlaunch(&lst, atoi_launcher);
+	loadlaunch(&lst, isalpha_launcher);
+	loadlaunch(&lst, isdigit_launcher);
+	loadlaunch(&lst, strcmp_launcher);
+	loadlaunch(&lst, strlen_launcher);
+	activate_launchers(&lst, argc, argv);
+	return (0);
+}
