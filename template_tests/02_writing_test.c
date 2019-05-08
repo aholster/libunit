34263ef5d83df57c9ebf1881ea9af6dee604d3a8
aholster <aholster@student.codam.nl>
2019-05-08 15:20:06 +0200

    Updated: 2019/05/08 15:20:39 by aholster      ########   odam.nl

--- template_tests/02_writing_test.c
+++ template_tests/02_writing_test.c
@@ -6,7 +6,7 @@
 /*   By: aholster <aholster@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/04/30 16:54:19 by aholster       #+#    #+#                */
-/*   Updated: 2019/05/03 16:29:23 by aholster      ########   odam.nl         */
+/*   Updated: 2019/05/08 15:19:51 by aholster      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,32 +17,17 @@ int	basic_writing(void)
 {
 	char	*output;
 	char	*input;
-	size_t	readsize;
-	ssize_t	status;
-	int		pipes[2];
+	size_t	b_read;
+	t_fds	fd_data;
 
-	readsize = 14;
 	input = "one";
 
-	if (pipe(pipes) == -1)
-		ft_error("pipes failure");
-	if (close(1) == -1)
-		ft_error("failed to close stdin");
-	if (dup2(pipes[1], 1) == -1)
-		ft_error("failed to reroute fd");
-	
+	capture_fd(1, &fd_data);
 	lu_putstr(input);
-
-	output = (char *)malloc(sizeof(char) * readsize + 1);
-	if (output == NULL)
-		ft_error("malloc failed");
-	status = read(pipes[0], output, readsize);
-	if (status == -1)
-		ft_error("read failure");
-	output[status] = '\0';
-
+	b_read = retrieve_text(&output, &fd_data);
+	reset_fd(&fd_data);
 	
-	dprintf(2, "strs: %s %s, amount read: %zu\n", output, input, status);
+	dprintf(2, "strs: %s %s, amount read: %zu\n", output, input, b_read);
 	if (strcmp(input, output) == 0)
 		return (0);
 	else
